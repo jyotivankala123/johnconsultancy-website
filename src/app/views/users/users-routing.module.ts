685105ec2223f8103cb2b3ec5d2dfc12c78feee0
252001eshaDas <77430277+252001eshaDas@users.noreply.github.com>
2024-04-20 01:57:04 +0530

    login ui

--- src/app/views/users/users-routing.module.ts
+++ src/app/views/users/users-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title:'Home'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title:'Login'
   }
 ];
 
