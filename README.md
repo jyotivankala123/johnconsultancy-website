8a3c3302b1167efa489d1eb7156cc50e6949b063
Eliza Hanslow <elizahanslow660@gmail.com>
2024-07-07 14:29:10 +0530

    upload dist 07-07-2024

--- README.md
+++
@@ -1,1 +0,0 @@
-# johnconsultancy-website
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  // apiURL: 'https://www.kawawamarketapi.acelance.com/',      
-  // imgUrl:'https://www.kawawamarketapi.acelance.com' ,
-  apiURL:'https://api.kawawamarket.com/',
-  imgUrl:'https://api.kawawamarket.com'
-};   
-   
